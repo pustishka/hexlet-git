a7828675d4d00a34c7a7dcb8c13220a4479461a3
Eduard Karol <korol94.94@gmail.com>
2022-04-28 21:07:40 +0300

    add vowels.py

---
+++ vowels.py
@@ -0,0 +1,28 @@
+vowels = {'a', 'e', 'i', 'o', 'u'}
+word = input('Provide a word to search for vowels: ')
+found=vowels.intersection(set(word))
+for vowel in found:
+    print(vowel)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#for letter in word:
+    #if letter in vowels:
+        #found.setdefault(letter,0)
+        #found[letter]+=1
+#for k,v in sorted(found.items()):
+    #print(k,'was found', v, 'time(s).')
+        
+
+        
