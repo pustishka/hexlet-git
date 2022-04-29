c1d00519979d9d7eefaf7d4e1dd11cf8ea65e18d
Eduard Karol <korol94.94@gmail.com>
2022-04-30 00:58:59 +0300

    update vowels

--- vowels.py
+++ vowels.py
@@ -1,9 +1,11 @@
-vowels = {'a', 'e', 'i', 'o', 'u'}
-word = input('Provide a word to search for vowels: ')
-found=vowels.intersection(set(word))
-for vowel in found:
-    print(vowel)
-
+def search4vowels(phrase:str)-> set:
+    """Выводим гласные, найденные во введенном слове."""
+    vowels = set('aeiou')
+    return vowels.intersection(set(phrase))
+def search4letters(phrase:str, letters:str='aeiou') -> set:
+    '''Возвращает множество букв из 'letters', найденных в указанной фразе.'''
+    return set(letters).intersection(set(phrase))
+    
 
 
 
